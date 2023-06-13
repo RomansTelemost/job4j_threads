ac0a21c026f603f17ab2ccbfc847b723cae8bbeb
Roman Koshelev <termopsis@mail.ru>
2023-06-13 22:14:36 +0300

    3. Состояние нити. [#229175 # [#229175]] - task

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,15 +1,30 @@
 package ru.job4j.concurrent;
 
+import javax.swing.plaf.nimbus.State;
+
 public class ThreadState {
+
+    private static final String NAMEFORMAT = "Thread name: %s, state %s";
+
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> { }
+                () -> { },
+                "firstThread"
         );
-        System.out.println(first.getState());
+        Thread second = new Thread(
+                () -> { },
+                "secondThread");
+        System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
+        System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
         first.start();
-        while (first.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getState());
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
+            System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
         }
-        System.out.println(first.getState());
+        System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
+        System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
+        System.out.println("Work has been completed");
     }
 }
