3b116caf8d0538176c1deff0e24d2bca0c3a64b2
Roman Koshelev <termopsis@mail.ru>
2023-06-14 07:40:50 +0300

    3. Состояние нити. [#229175 # [#229175 #422900]] - task - fix1

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -4,7 +4,7 @@ import javax.swing.plaf.nimbus.State;
 
 public class ThreadState {
 
-    private static final String NAMEFORMAT = "Thread name: %s, state %s";
+    private static final String NAME_FORMAT = "Thread name: %s, state %s";
 
     public static void main(String[] args) {
         Thread first = new Thread(
@@ -14,17 +14,17 @@ public class ThreadState {
         Thread second = new Thread(
                 () -> { },
                 "secondThread");
-        System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
-        System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
+        System.out.println(String.format(NAME_FORMAT, first.getName(), first.getState()));
+        System.out.println(String.format(NAME_FORMAT, second.getName(), second.getState()));
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
                 || second.getState() != Thread.State.TERMINATED) {
-            System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
-            System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
+            System.out.println(String.format(NAME_FORMAT, first.getName(), first.getState()));
+            System.out.println(String.format(NAME_FORMAT, second.getName(), second.getState()));
         }
-        System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
-        System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
+        System.out.println(String.format(NAME_FORMAT, first.getName(), first.getState()));
+        System.out.println(String.format(NAME_FORMAT, second.getName(), second.getState()));
         System.out.println("Work has been completed");
     }
 }
