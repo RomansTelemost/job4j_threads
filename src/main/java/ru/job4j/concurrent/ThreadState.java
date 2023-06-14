f4f7edec31791d1566a738fc9ce873782e294152
Roman Koshelev <termopsis@mail.ru>
2023-06-14 07:40:26 +0300

    3. Состояние нити. [#229175 # [#229175 #422900]] - task - fix1

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -19,7 +19,7 @@ public class ThreadState {
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println(String.format(NAMEFORMAT, first.getName(), first.getState()));
             System.out.println(String.format(NAMEFORMAT, second.getName(), second.getState()));
         }
