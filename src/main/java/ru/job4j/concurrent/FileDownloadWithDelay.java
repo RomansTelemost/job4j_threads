72987e5263979288510e3a0891e0f51289f45402
Roman <termopsis@mail.ru>
2023-06-25 19:34:55 +0700

    8. Скачивание файла с ограничением. [#144271 # [#144271]] - task - validation

--- src/main/java/ru/job4j/concurrent/FileDownloadWithDelay.java
+++ src/main/java/ru/job4j/concurrent/FileDownloadWithDelay.java
@@ -1,7 +1,6 @@
 package ru.job4j.concurrent;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
@@ -10,7 +9,7 @@ public class FileDownloadWithDelay {
 
     public static void main(String[] args) throws Exception {
         String url = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
-        try(BufferedInputStream bis = new BufferedInputStream(new URL(url).openStream())) {
+        try (BufferedInputStream bis = new BufferedInputStream(new URL(url).openStream())) {
             FileOutputStream fos = new FileOutputStream("data/files/test1.txt");
             byte[] fileBuff = new byte[1024];
             int bytesRead;
