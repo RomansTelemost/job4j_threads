d05fafa5c179775f6d6f704687a3f87f8bacef0b
Roman <termopsis@mail.ru>
2023-07-05 09:29:36 +0700

    2. Модель памяти Java [#267917 # [#267917]] - task

---
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+public class DCLSingleton {
+
+    /**
+     * inst переменная может читаться двумя потоками. Т.е. Первый изменил, а второй должен увидеть эти изменения.
+     * Т.е. читать каждый раз значение из регистра, не ссылаясь на значение загруженного в cpu cache.
+     * Для этого укажем модификатор volatile
+     */
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+}
