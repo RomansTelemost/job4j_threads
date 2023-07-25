37481513b8605c09570338da9a59561f148db3cf
Roman <termopsis@mail.ru>
2023-07-25 23:16:57 +0300

    test Synch semaphore

--- src/main/java/ru/job4j/synch/SemaphoreExample.java
+++ src/main/java/ru/job4j/synch/SemaphoreExample.java
@@ -1,5 +1,25 @@
 package ru.job4j.synch;
 
+import java.util.concurrent.Semaphore;
+
 public class SemaphoreExample {
 
+    public static void main(String[] args) throws InterruptedException{
+        Semaphore sem = new Semaphore(1);
+        Runnable task = () -> {
+            try {
+                sem.acquire(2);
+                System.out.println("Нить выполнила задачу");
+                sem.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        new Thread(task).start();
+        Thread.sleep(3000);
+        sem.release(1);
+        System.out.println("sa");
+        Thread.sleep(3000);
+        sem.release(1);
+    }
 }
