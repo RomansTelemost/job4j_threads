72e25d0598b81afead7962cf67bb6e40edabc004
Roman <termopsis@mail.ru>
2023-08-02 03:39:39 +0700

    0. Управление нитью через wait. [#6858 # [#6858]] - task

---
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+public class CountBarrier {
+
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public synchronized void count() {
+        count++;
+        notifyAll();
+    }
+
+    public synchronized void await() throws InterruptedException {
+        while (count < total) {
+            wait();
+        }
+    }
+}
