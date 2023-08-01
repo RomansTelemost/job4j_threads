c64e880c37d9363af30735e57143895a23c97960
Roman <termopsis@mail.ru>
2023-08-01 21:42:21 +0300

    CyclicBarrierExample and PhaserExample

---
+++ src/main/java/ru/job4j/synch/CyclicBarrierExample.java
@@ -0,0 +1,8 @@
+package ru.job4j.synch;
+
+public class CyclicBarrierExample {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/ru/job4j/synch/PhaserExample.java
@@ -0,0 +1,10 @@
+package ru.job4j.synch;
+
+public class PhaserExample {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
