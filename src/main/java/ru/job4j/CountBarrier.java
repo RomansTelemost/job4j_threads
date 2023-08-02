fb47c996516e5dbb596e365c1023ae48be3de8a5
Roman <termopsis@mail.ru>
2023-08-02 20:59:04 +0300

    0. Управление нитью через wait. [#6858 # [#6858 #434036]] - add main method

--- src/main/java/ru/job4j/CountBarrier.java
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -12,14 +12,42 @@ public class CountBarrier {
         this.total = total;
     }
 
+    public int getCount() {
+        return count;
+    }
+
     public synchronized void count() {
         count++;
         notifyAll();
     }
 
-    public synchronized void await() throws InterruptedException {
+    public synchronized void await() {
         while (count < total) {
-            wait();
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
+
+    public static void main(String[] args) {
+        CountBarrier countBarrier = new CountBarrier(10);
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 15; i++) {
+                countBarrier.count();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            countBarrier.await();
+            System.out.println("Работа завершена т.к. счетчик стал больше " + (countBarrier.getCount() - 1));
+        });
+        thread1.start();
+        thread2.start();
     }
 }
