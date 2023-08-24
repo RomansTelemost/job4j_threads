d4163662966d40c1e659dcd4ac7a0bd7ddd9b970
Roman <termopsis@mail.ru>
2023-08-24 22:39:32 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,21 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        memory.computeIfAbsent(model.getId(), key -> model);
+        return false;
+    }
+
+    public void delete(Base model) {
+    }
+}
